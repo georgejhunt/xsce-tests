5c3c4cc6f08d0133bf11af11cb58d0917bde34bc
George Hunt <georgejhunt@gmail.com>
2016-01-25 09:32:28 -0800

    add ini2map.py back in too

---
+++ Run-on-XO/testing/ini2map.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Read STDIN parsing it as an INI file configuration and emit the same
+as a BASH 4 map. Usage in BASH 4 scripts:
+    declare -A config=`cat cfg.ini | ./ini2bash.py database`
+    echo ${config["dbname"]}
+"""
+
+import sys
+import ConfigParser
+
+
+def parse_ini():
+    config = ConfigParser.RawConfigParser()
+    config.readfp(sys.stdin)
+    return config
+
+
+def emit_bashmap(config):
+    print '(',
+    for sec in config.sections():
+        for pair in config.items(sec):
+            k, v = pair
+            if v[0] == '"':
+                print '["%s_%s"]=%s' % (sec, k, v),
+            else:
+                print '["%s_%s"]="%s"' % (sec, k, v),
+    print ')'
+
+
+def main():
+    config = parse_ini()
+    emit_bashmap(config)
+
+if __name__ == "__main__":
+    main()
