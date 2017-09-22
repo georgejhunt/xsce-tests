5512d0e4a551a00099ed002c13d35b2da42d7cba
George Hunt <ghunt@hadron.lan>
2017-09-22 16:01:54 -0700

    work towards autotest on hadron

--- vbox-tester/vms/debian9/debian9-vm.sh
+++ vbox-tester/vms/debian9/debian9-vm.sh
@@ -4,14 +4,14 @@
 # first remove any lingering set vm
 vboxmanage controlvm debian9-test poweroff
 vboxmanage unregistervm debian9-test --delete
-vboxmanage clonevm debian9-starter --register --name debian9-test
+vboxmanage clonevm debian9-min --register --name debian9-test
 vboxmanage  modifyvm debian9-test --natpf1 "ssh,tcp,,5022,,22"
 vboxmanage startvm debian9-test --type headless
 
 YMD=`date +"%y%m%d-%H:%M"`
-scp -P 5022 ../../scripts/iiab.sh localhost:/root/iiab.sh
+scp -P 5022 ../../scripts/iiab.sh root@localhost:/root/iiab.sh
 # copy the desired local_vars.yml
-scp -P 5022 ../../scripts/loca_vars.yml localhost:/root/local_vars.yml
+scp -P 5022 ../../scripts/loca_vars.yml root@localhost:/root/local_vars.yml
 # execute the following remotely on the VM
-time ssh -p 5022 localhost '/root/iiab.sh > /root/output.log'
-scp -P 5022 localhost:/root/output.log ./debian9-$YMD.log
+time ssh -p 5022 root@localhost '/root/iiab.sh > /root/output.log'
+scp -P 5022 root@localhost:/root/output.log ./debian9-$YMD.log
